f1ee7cdb45ac53aab41554236953867fa0ad092e
Ben Chapman <5124569+Ben-Chapman@users.noreply.github.com>
2019-06-12 08:00:18 -0400

    Build enhancements

--- build-firmware.sh
+++ build-firmware.sh
@@ -3,17 +3,23 @@
 # Quick helper script to build QMK firmware
 PWD=$(pwd)
 
-QMK_DIR='/home/benchapman/src/qmk_firmware'
+QMK_DIR="/home/ben/src/qmk_firmware"
 
 cd ${QMK_DIR}
 
 if [[ $1 == 'clean' ]]; then
-    make clean
-elif [[ $1 == '--help' ]]; then
+    make clean &&
+    make televideo2k:default
+elif [[ $1 == 'help' ]]; then
     echo -e "$0        : Compile firmware, reusing prior build artifacts."
     echo -e "$0 clean  : Remove all prior build artifacts before compiling anew."
+    echo -e "$0 flash  : Compile firmware and proceed to flash to a Teensy"
     exit 5
+elif [[ $1 == 'flash' ]]; then
+    make televideo2k:default:teensy
+else
+    # Now compile
+    make televideo2k:default
 fi
 
-# Now compile
-make televideo2k:default
+
